c90ff319f1550ca14ad5f124c296ced866e6144a
Jimmy Sanford <jimbo@users.noreply.github.com>
2021-10-04 18:33:56 -0700

    Fix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,34 @@ const venia = require("./theme")
 const { getColors } = require("./theme/lib/colors")
 const defaults = require("./theme/lib/defaults")
 
+const COLORS = {
+	brand: {
+		400: "248 123 21"
+	},
+	neutral: {
+		50: "255 255 255",
+		75: "250 250 250",
+		100: "245 245 245",
+		200: "234 234 234",
+		300: "225 225 225",
+		400: "202 202 202",
+		500: "179 179 179",
+		600: "142 142 142",
+		700: "110 110 110",
+		800: "75 75 75",
+		900: "44 44 44"
+	},
+	static: {
+		black: "0 0 0",
+		white: "255 255 255"
+	}
+}
+
 const config = {
 	...defaults,
 	presets: [venia],
 	theme: {
-		colors: getColors({
-			brand: {
-				400: "38 128 235"
-			},
-			neutral: {
-				50: "255 255 255",
-				75: "250 250 250",
-				100: "245 245 245",
-				200: "234 234 234",
-				300: "225 225 225",
-				400: "202 202 202",
-				500: "179 179 179",
-				600: "142 142 142",
-				700: "110 110 110",
-				800: "75 75 75",
-				900: "44 44 44"
-			},
-			static: {
-				black: "0 0 0",
-				white: "255 255 255"
-			}
-		}),
+		colors: getColors(COLORS),
 		fontFamily: {
 			sans: ["Avenir Next", "ui-sans-serif", "sans-serif"],
 			serif: ["Avenir Next", "ui-serif", "serif"]
@@ -37,6 +39,9 @@ const config = {
 				header: {
 					// backgroundColor: theme("colors.neutral.900"),
 					// color: theme("colors.neutral.50")
+				},
+				root: {
+					// colors: COLORS
 				}
 			}
 		})
